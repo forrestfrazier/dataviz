5846b0b39996e3cd912c876f892a6df8f67a897a
forrestfrazier <forrestfrazier@gmail.com>
2014-06-25 13:33:07 -0400

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+var trafficLights = ["green", "yellow", "red"];
+
+function drawGrid(configuration, data) {
+
+    // Add templates for columns as necessary
+    $.each(configuration.columns, function(index, column) {
+        switch(column.type) {
+        case "trafficLight":
+            column.template = function(dataItem) {
+                return '<div class="' + trafficLights[dataItem[column.field]] + '">&nbsp;</div>';
+            };
+        break;
+        case "barChart":
+            column.template = function(dataItem) {
+                return '<div class="blue-vlight" style="width: ' + dataItem[column.field] + '%">&nbsp;</div>';
+            };
+        break;
+        }
+    });
+
+    configuration.dataSource = new kendo.data.DataSource({
+        data: data
+    });
+
+    $("#grid").empty().kendoGrid(configuration);
+}
+
+function handleSelection(value) {
+    var filePair = [
+        { config: "data/config1-text.json", data: "data/data1.json" },
+        { config: "data/config1-dataviz.json", data: "data/data1.json" },
+        { config: "data/config2.json", data: "data/data2.json" },
+    ];
+
+    $.getJSON(filePair[value].config)
+        .done(function(configuration) {
+            $.getJSON(filePair[value].data)
+                .done(function(data) {
+                    drawGrid(configuration, data);
+                });
+        });
+}
+
+$(document).ready(function () {
+
+    $(".use-case-selector").on("change", function(event) {
+        var el = event.target;
+        handleSelection(el.options[el.selectedIndex].value);
+    });
+
+    handleSelection(0);
+});
