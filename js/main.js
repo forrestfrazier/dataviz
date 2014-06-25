74a95e7c5d7e63bd4a43f4e32aff1b8de1a2282e
forrestfrazier <forrestfrazier@gmail.com>
2014-06-25 15:28:44 -0400

    json updates

--- js/main.js
+++ js/main.js
@@ -4,17 +4,23 @@ function drawGrid(configuration, data) {
 
     // Add templates for columns as necessary
     $.each(configuration.columns, function(index, column) {
-        switch(column.type) {
-        case "trafficLight":
-            column.template = function(dataItem) {
-                return '<div class="' + trafficLights[dataItem[column.field]] + '">&nbsp;</div>';
-            };
-        break;
-        case "barChart":
-            column.template = function(dataItem) {
-                return '<div class="blue-vlight" style="width: ' + dataItem[column.field] + '%">&nbsp;</div>';
-            };
-        break;
+        switch (column.type) {
+            case "trafficLight":
+                column.template = function(dataItem) {
+                    return '<div class="' + trafficLights[dataItem[column.field]] + '">&nbsp;</div>';
+                };
+                break;
+            case "barChart":
+                column.template = function(dataItem) {
+                    return '<div class="blue-vlight" style="width: ' + dataItem[column.field] + '%">&nbsp;</div>';
+                };
+                break;
+            case "statusTextBar":
+                column.template = function(dataItem) {
+                    return '<span>/#:data.completed# %</span>'
+                    return '<div class="blue-vlight" style="width: ' + dataItem[column.field] + '%">&nbsp;</div>';
+                };
+                break;
         }
     });
 
@@ -25,12 +31,21 @@ function drawGrid(configuration, data) {
     $("#grid").empty().kendoGrid(configuration);
 }
 
+// show the display based on dropdown
 function handleSelection(value) {
-    var filePair = [
-        { config: "data/config1-text.json", data: "data/data1.json" },
-        { config: "data/config1-dataviz.json", data: "data/data1.json" },
-        { config: "data/config2.json", data: "data/data2.json" },
-    ];
+    var filePair = [{
+        config: "data/config1-text.json",
+        data: "data/data1.json"
+    }, {
+        config: "data/config1-dataviz.json",
+        data: "data/data1.json"
+    }, {
+        config: "data/config2.json",
+        data: "data/data2.json"
+    }, {
+        config: "data/config3.json",
+        data: "data/data3.json"
+    }, ];
 
     $.getJSON(filePair[value].config)
         .done(function(configuration) {
@@ -41,7 +56,7 @@ function handleSelection(value) {
         });
 }
 
-$(document).ready(function () {
+$(document).ready(function() {
 
     $(".use-case-selector").on("change", function(event) {
         var el = event.target;
